50bd9faab3b98e08774836694d8a802eded2a49d
adeng21 <adeng21@gmail.com>
2014-04-01 15:45:25 -0400

    initial commit with gems and rspec install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gemmerator::Application.config.secret_key_base = '783d1448ab0327b93ad2bbfdf473a10af5c10ef2c89b6d9d46d5cd2125a331f7adf0c243790e1f7f4acb18588d5f012231e000bf55fd475f14f10092b9c5e151'
