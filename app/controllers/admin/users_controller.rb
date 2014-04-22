c7934fce54a0faafb765eda4f55a549dcfcee66e
Michael <michael.boerger@gmail.com>
2014-04-22 12:02:13 -0400

    Fixed admin notes

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,20 +2,13 @@ module Admin
   class UsersController < ApplicationController
     before_action :authenticate_admin
     def index
-      @user = User.all
+      @users = User.all
     end
 
     def destroy
       @user = User.find(params[:id])
       @user.delete
       redirect_to admin_users_path
-    end
-
-    protected
-    def authenticate_admin
-      if !current_user || current_user.admin? != true
-        render 'public/404.html'
-      end
     end
   end
 end
